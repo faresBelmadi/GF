5e428bd6499f27c2f3af408dd9f7727616b990bf
xSternLeVegetal <quentin.le.masson@outlook.com>
2023-09-04 21:01:45 +0200

    add stats in autel

--- Assets/Scripts/Autel/AutelManager.cs
+++ Assets/Scripts/Autel/AutelManager.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using Unity.Jobs;
 using UnityEditor.Experimental.GraphView;
 using UnityEngine.SceneManagement;
+using static System.Net.Mime.MediaTypeNames;
+using Image = UnityEngine.UI.Image;
 
 public class AutelManager : MonoBehaviour
 {
@@ -23,6 +25,9 @@ public class AutelManager : MonoBehaviour
     public Button BuyButton;
 
     public Button RetourButton;
+
+    public TextMeshProUGUI ValeurRadiance, ValeurFA, ValeurVitesse, ValeurConviction, ValeurResilience, ValeurCalme, ValeurVolonter, ValeurConscience, ValeurClairvoyance;
+    public TextMeshProUGUI ModifRadiance, ModifFA, ModifVitesse, ModifConviction, ModifResilience, ModifCalme, ModifVolonter, ModifConscience, ModifClairvoyance;
 
     void FixedUpdate()
     {
@@ -52,6 +57,25 @@ public class AutelManager : MonoBehaviour
                 AllSpellsIcon[capa.Spell.IDSpell].GetComponent<Image>().sprite = capa.Spell.Sprite;
             //spellIcon.gameObject.GetComponent<Image>().sprite = GameManager.instance.playerStat.ListSpell[0].Sprite;
         }
+
+        SetUpStatsDescription();
+    }
+
+    public void SetUpStatsDescription()
+    {
+        var stats = GameManager.instance.classSO.PlayerStat;
+        ValeurRadiance.text = stats.RadianceMax.ToString();
+        ValeurFA.text = stats.ForceAme.ToString(); 
+        ValeurVitesse.text = stats.VitesseOriginal.ToString(); 
+        ValeurConviction.text = stats.ConvictionOriginal.ToString(); 
+        ValeurResilience.text = stats.Resilience.ToString(); 
+        ValeurCalme.text = stats.Calme.ToString();
+        ValeurVolonter.text = stats.Volonter.ToString();
+        ValeurConscience.text = stats.Conscience.ToString(); 
+        ValeurClairvoyance.text = stats.Clairvoyance.ToString();
+
+        ModifRadiance.text = ModifFA.text = ModifVitesse.text = ModifConviction.text = ModifResilience.text =
+            ModifCalme.text = ModifVolonter.text = ModifConscience.text = ModifClairvoyance.text = "";
     }
 
     public void SelectSpell(int Id)
@@ -65,6 +89,7 @@ public class AutelManager : MonoBehaviour
 
                 DescriptionSpellText.text = capa.Spell.Description;
                 CostCapaText.text = "cout : " + capa.EssenceCost;
+                ModifStatCapa(capa);
                 if (capa.EssenceCost <= GameManager.instance.playerStat.Essence)
                 {
                     BuyButton.onClick.AddListener(delegate { BuyCapa(capa); });
@@ -76,6 +101,74 @@ public class AutelManager : MonoBehaviour
                     BuyButton.GetComponent<Image>().color = Color.gray;
                 }
             }
+        }
+    }
+
+    public void ModifStatCapa(Competence capa)
+    {
+        ModifRadiance.text = ModifFA.text = ModifVitesse.text = ModifConviction.text = ModifResilience.text =
+            ModifCalme.text = ModifVolonter.text = ModifConscience.text = ModifClairvoyance.text = "";
+        foreach (var modifStat in capa.ModifStat)
+        {
+            var value = modifStat.Valeur.ToString();
+            Color colorTxt;
+            if (modifStat.Valeur < 0)
+            {
+                colorTxt = Color.red;
+            }
+            else
+            {
+                colorTxt = Color.green;
+            }
+            switch (modifStat.StatModif)
+            {
+                case StatModif.Calme:
+                    ModifCalme.text = value;
+                    ModifCalme.color = colorTxt;
+                    ModifCalme.faceColor = colorTxt;
+                    break;
+                case StatModif.Clairvoyance:
+                    ModifClairvoyance.text = value;
+                    ModifClairvoyance.color = colorTxt;
+                    ModifClairvoyance.faceColor = colorTxt;
+                    break;
+                case StatModif.ConscienceMax:
+                    ModifConscience.text = value;
+                    ModifConscience.color = colorTxt;
+                    ModifConscience.faceColor = colorTxt;
+                    break;
+                case StatModif.Conviction:
+                    ModifConviction.text = value;
+                    ModifConviction.color = colorTxt;
+                    ModifConviction.faceColor = colorTxt;
+                    break;
+                case StatModif.ForceAme:
+                    ModifFA.text = value;
+                    ModifFA.color = colorTxt;
+                    ModifFA.faceColor = colorTxt;
+                    break;
+                case StatModif.RadianceMax:
+                    ModifRadiance.text = value;
+                    ModifRadiance.color = colorTxt;
+                    ModifRadiance.faceColor = colorTxt;
+                    break;
+                case StatModif.Resilience:
+                    ModifResilience.text = value;
+                    ModifResilience.color = colorTxt;
+                    ModifResilience.faceColor = colorTxt;
+                    break;
+                case StatModif.VolonterMax:
+                    ModifVolonter.text = value;
+                    ModifVolonter.color = colorTxt;
+                    ModifVolonter.faceColor = colorTxt;
+                    break;
+                case StatModif.Vitesse:
+                    ModifVitesse.text = value;
+                    ModifVitesse.color = colorTxt;
+                    ModifVitesse.faceColor = colorTxt;
+                    break;
+            }
+                
         }
     }
 
