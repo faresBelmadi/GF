1e6aaad06b72098cf02b4d062f34d28186e5b2b7
kohotatsu <158819634+kohotatsu@users.noreply.github.com>
2024-07-17 00:03:04 +0200

    Pulse Player Tension
    Adding shader + material + script system For a pulsed bloom
    Adding it to the player tension bar.
    Triger The looping pulse bloom when tensionBar is full and stoping it otherwise.

---
+++ Assets/Scripts/PulseBloom_System.cs
@@ -0,0 +1,92 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using TMPro;
+using UnityEngine;
+
+using UnityEngine.UI;
+public class PulseBloom_System : MonoBehaviour
+{
+
+
+    [SerializeField]
+    float bloomDuration = 1f;
+    [SerializeField]
+    public bool loop = false;
+    [SerializeField]
+    float bloomIntensity = 1f;
+    [SerializeField]
+    AnimationCurve bloomIntensityShape;
+    [SerializeField]
+    private Gradient bloomGradient;
+
+    [SerializeField]
+    private Material bloomMaterial;
+    private Coroutine bloomRoutine;
+    private CultureInfo en_us = CultureInfo.GetCultureInfo("en-US");
+    private void Start()
+    {
+        bloomMaterial.SetFloat("_Intensity", .5f);
+        //TriggerBloom();
+    }
+    public void TriggerBloom(bool doOverride = false)
+    {
+        if (bloomRoutine != null)
+        {
+            if (doOverride) StopCoroutine(bloomRoutine);
+            else return;
+        }
+        bloomRoutine = StartCoroutine(BloomingRoutine());
+    }
+    private IEnumerator BloomingRoutine()
+    {
+        float timePassed = 0f;
+
+        do
+        {
+            float applyedIntensity = bloomIntensityShape.Evaluate(timePassed / bloomDuration) * bloomIntensity;
+            bloomMaterial.SetFloat("_Intensity", applyedIntensity);
+            bloomMaterial.SetColor("_Color", bloomGradient.Evaluate(timePassed / bloomDuration));
+
+            timePassed += Time.deltaTime;
+            yield return null;
+
+        } while (timePassed < bloomDuration);
+        bloomMaterial.SetFloat("_Intensity", .5f);
+        bloomMaterial.SetColor("_Color", Color.white);
+        bloomRoutine = null;
+        if (loop) { TriggerBloom(); }
+    }
+    //Duration
+    public void OnDurationValueChanged(float duration)
+    {
+        UpdateDurationValue(duration);
+    }
+    public void OnDurationValueChanged(string duration)
+    {
+        UpdateDurationValue(float.Parse(duration, en_us));
+    }
+    private void UpdateDurationValue(float duration)
+    {
+        bloomDuration = duration;
+    }
+    //Loop
+    public void OnToggleLoop()
+    {
+        loop = !loop;
+    }
+    //Intensity
+    public void OnIntensityValueChanged(float intensity)
+    {
+        UpdateIntensityValue(intensity);
+    }
+    public void OnIntensityValueChanged(string intensity)
+    {
+        UpdateIntensityValue(float.Parse(intensity, en_us));
+    }
+    private void UpdateIntensityValue(float intensity)
+    {
+        bloomIntensity = intensity;
+    }
+
+}
