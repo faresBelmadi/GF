dfd63f916218c9147d544eddaf08479af833ed9c
NikoMyoji <nicolas.vlte@gmail.com>
2024-07-16 17:11:31 +0200

    Affichage Gain d'essence
    Modification du prefab pour afficher le heal et le gain d'essence

--- Assets/Scripts/Combat/EssenceUI.cs
+++ Assets/Scripts/Combat/EssenceUI.cs
@@ -1,14 +1,27 @@
-﻿using UnityEngine;
+﻿using TMPro;
+using UnityEngine;
 
 public class EssenceUI : MonoBehaviour
 {
     public GameObject buttonConsumation;
+
+    [SerializeField]
+    private TMP_Text _valeurText;
+    [SerializeField]
+    private Essence _essenceRootObject;
+
+    private void Start()
+    {
+        buttonConsumation.GetComponentInChildren<TMP_Text>().text += $" ({_essenceRootObject.Heal})";
+        _valeurText.text = _essenceRootObject.amount.ToString();
+    }
 
     public void activate()
     {
         if (TutoManager.Instance != null && !TutoManager.Instance.ShowSoulConsumation)
             return;
         buttonConsumation.SetActive(true);
+        _valeurText.gameObject.SetActive(true);
     }
 
     public void deactivate()
@@ -20,5 +33,6 @@ public class EssenceUI : MonoBehaviour
     private void invokedDeactivate()
     {
         buttonConsumation.SetActive(false);
+        _valeurText.gameObject.SetActive(false);
     }
 }
