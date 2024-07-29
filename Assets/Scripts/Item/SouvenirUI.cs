17c17e6ceb9f7ee87a6a2ce0165c9fc3ad7ab005
NikoMyoji <nicolas.vlte@gmail.com>
2024-07-29 15:21:39 +0200

    Set new Souvenir prefab & fix tuto issue

--- Assets/Scripts/Item/SouvenirUI.cs
+++ Assets/Scripts/Item/SouvenirUI.cs
@@ -1,5 +1,7 @@
-﻿using TMPro;
+﻿using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.Rendering;
 using UnityEngine.UI;
 
 [System.Serializable]
@@ -8,12 +10,33 @@ public class SouvenirUI : MonoBehaviour
     public Souvenir LeSouvenir;
     public GameObject SouvenirObject;
     public TextMeshProUGUI TexteDescription;
+    [Tooltip("Set the rarity border, starting with 0 = most common")]
+    [SerializeField]
+    private List<GameObject> _rarityBorders;
 
     public void StartUp()
     {
         SouvenirObject = this.gameObject;
         SouvenirObject.GetComponent<Image>().sprite = LeSouvenir.Icon;
         TexteDescription.text = LeSouvenir.Nom + "\n" + DescriptionEmotion() + "\n" + "Slots : " + LeSouvenir.Slots.ToString() + "\n" + LeSouvenir.Description;
+        SetRarityBorder(LeSouvenir.Rarete);
+    }
+
+    private void SetRarityBorder(Rarity rarity)
+    {
+        for (int i=0; i < _rarityBorders.Count;i++)
+        {
+            _rarityBorders[i].SetActive(false);
+        }
+        if ((int)rarity>= _rarityBorders.Count)
+        {
+            Debug.LogError($"Rarity {LeSouvenir.Rarete} for souvenir {LeSouvenir.name} not supported");
+            _rarityBorders[0].SetActive(true);
+        }
+        else
+        {
+            _rarityBorders[(int)LeSouvenir.Rarete].SetActive(true);
+        }
     }
 
     private string DescriptionEmotion()
