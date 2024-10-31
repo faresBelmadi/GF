71a8d6b72352df5a4730668957b9b52d5527b06d
kohotatsu <158819634+kohotatsu@users.noreply.github.com>
2024-10-31 01:53:29 +0100

    Add General PopUp
    Adding a general PopUp SInglton to call InvokePopUp from anywhere.
    The popUp manage a queue, and pop ups have a title, a core text and staying durration.

--- Assets/Scripts/Combat/BattleManager.cs
+++ Assets/Scripts/Combat/BattleManager.cs
@@ -541,6 +541,14 @@ public class BattleManager : MonoBehaviour
         currentIdTurn = key;
         if (key == idPlayer)
         {
+            //TEST FOR GENERAL POPUP
+            GeneralPopUp.Instance.InvokePopUp("Start Player Turn","this is th message describing the pop Up");
+            GeneralPopUp.Instance.InvokePopUp("Test PopUp Queue 1","With a short Delay",1f);
+            GeneralPopUp.Instance.InvokePopUp("Test PopUp Queue 2", "Lorem ipsum odor amet, consectetuer adipiscing elit. Feugiat lacinia sagittis id mi commodo gravida. Pretium tortor in facilisis ullamcorper sem.", 5f);
+            GeneralPopUp.Instance.InvokePopUp("Test PopUp Queue 3","With a short Delay",1f);
+            Debug.Log("Start Player Turn (PopUp Test)");
+            //END TEST
+
             player.StartTurn(nbPhase<2);
             battleUI.textPLayingTurn.text = (GameManager.Instance!=null)?GameManager.Instance.classSO.NameClass:TutoManager.Instance.TutoClassSo.NameClass;
         }
---
+++ Assets/Scripts/GeneralPopUp.cs
@@ -0,0 +1,137 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Drawing.Text;
+using Unity.Collections;
+using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
+
+public class GeneralPopUp : MonoBehaviour
+{
+    private static GeneralPopUp instance = null;
+    public static GeneralPopUp Instance = instance;
+    private Canvas activeCanvas = null;
+    [Header("PopUp Window")]
+    [SerializeField] private GameObject pupUpWindowPrefab;
+    [SerializeField] private AnimationCurve movmentCurve;
+    //[SerializeField] private Vector2 startPos;
+    //[SerializeField] private Vector2 endPos;
+    [SerializeField] private float popAnimDuration;
+
+    private bool isActivePopUpPresent = false;
+    private List<PopUpInfo> popUpQueue = new List<PopUpInfo>();
+
+    class PopUpInfo
+    {
+        public string title;
+        public string core;
+        public float activeTime;
+        public PopUpInfo(string _title, string _core, float _activeTime) 
+        {
+            title = _title;
+            core = _core;
+            activeTime = _activeTime;
+        }
+    }
+
+    private Coroutine PopUpMovmentsCoroutine = null;
+
+    private void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Debug.Log("Already Instancied");
+            Destroy(this);
+            return;
+        }
+        else
+        {
+            instance = this;
+            Instance = instance;
+        }
+        DontDestroyOnLoad(this.gameObject);
+        InvokePopUp();
+    }
+    private GameObject GetcurrentPertinantCanvas()
+    {
+        foreach (GameObject canv in GameObject.FindGameObjectsWithTag("CanvasLayer"))
+        {
+            if(canv.GetComponent<Canvas>() != null 
+                && canv.activeInHierarchy 
+                && canv.GetComponent<Canvas>().sortingLayerName == "UI")
+            {
+                Debug.Log("Pertinant canvas found");
+                return canv;
+            }
+        }
+        Debug.Log("RETURN NULL: No Pertiannt canvas found");
+        return null;
+    }
+    public void ResetPopUpQueue()
+    {
+        popUpQueue.Clear();
+    }
+    public void InvokePopUp(string title = "", string core = "", float activeTime = 3f)
+    {
+
+        PopUpInfo popUp = new PopUpInfo(title, core, activeTime);
+        if (PopUpMovmentsCoroutine != null)
+        {
+            popUpQueue.Add(popUp);
+            return;
+        }
+        isActivePopUpPresent = true;
+        PopUpMovmentsCoroutine = StartCoroutine(InvokePopUpCoroutine(popUp));
+    }
+
+    IEnumerator InvokePopUpCoroutine(PopUpInfo popUp = null)
+    {
+        Vector3 endpos = Vector2.zero;
+        Vector3 startPos = Vector2.zero;
+
+
+        while (popUpQueue.Count > 0 || popUp != null)
+        {
+            if(popUp == null)
+            {
+                popUp = popUpQueue[0];
+                popUpQueue.RemoveAt(0);
+            }
+
+            GameObject choosedCanvas = GetcurrentPertinantCanvas();
+            GameObject popUpWindowObject = Instantiate(pupUpWindowPrefab, choosedCanvas.transform);
+            popUpWindowObject.GetComponent<PopUpManager>().SetTitleText(popUp.title);
+            popUpWindowObject.GetComponent<PopUpManager>().SetCoreText(popUp.core);
+            RectTransform popUpRect = popUpWindowObject.GetComponent<RectTransform>();
+            
+            endpos = popUpRect.anchoredPosition;
+            startPos = endpos;
+            //Pop IN
+            startPos.x = endpos.x - popUpRect.sizeDelta.x;
+            popUpRect.anchoredPosition = startPos;
+            float timeLeft = 0;
+            while (timeLeft < popAnimDuration)
+            {
+                timeLeft += Time.deltaTime;
+                popUpRect.anchoredPosition = startPos + (movmentCurve.Evaluate(timeLeft/popAnimDuration) * (endpos - startPos)); //Vector3.Lerp(startPos, endpos, movmentCurve.Evaluate(timeLeft));
+                yield return null;
+            }
+            popUpRect.anchoredPosition = endpos;
+
+            yield return new WaitForSeconds(popUp.activeTime);
+
+            //Pop OUT
+            while (timeLeft > 0)
+            {
+                timeLeft -= Time.deltaTime;
+                popUpRect.anchoredPosition = startPos + (movmentCurve.Evaluate(timeLeft/ popAnimDuration) * (endpos - startPos)); //Vector3.Lerp(startPos, endpos, movmentCurve.Evaluate(timeLeft));
+                yield return null;
+            }
+            popUpRect.anchoredPosition = startPos;
+
+            Destroy(popUpWindowObject);
+            popUp = null;
+        }
+        PopUpMovmentsCoroutine = null;
+        yield break;
+    }
+}
