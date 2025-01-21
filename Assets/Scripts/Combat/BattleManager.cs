6f7e5a4777a0d0da1bf255e16d4d4aa5e07b643b
faresBelmadi <faresBelmadi@users.noreply.github.com>
2025-01-21 22:35:04 +0800

    Update intention + fix spell non buyable

--- Assets/Scripts/Combat/BattleManager.cs
+++ Assets/Scripts/Combat/BattleManager.cs
@@ -255,6 +255,10 @@ public class BattleManager : MonoBehaviour
         GameManager.OnStartDialog -= CombatEnableSetup;
     }
 
+    public int getJoueurClairvoyance()
+    {
+        return player.Stat.Clairvoyance;
+    }
     void DialogueEnableSetup()
     {
         player.InitRefBattleMan(this);
