e3409d97eec8490d2402526ef161a821ced5291d
NikoMyoji <nicolas.vlte@gmail.com>
2025-01-24 14:44:16 +0100

    Prepare for Build 0.2.3

--- Assets/Scripts/Combat/Behavior/JoueurBehavior.cs
+++ Assets/Scripts/Combat/Behavior/JoueurBehavior.cs
@@ -1,10 +1,8 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
-using static UnityEditor.Progress;
 
 public class JoueurBehavior : CombatBehavior
 {
