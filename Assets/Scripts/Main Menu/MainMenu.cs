747372cb19f61a5df6a3d02a07b5ef3a600ff00f
NikoMyoji <nicolas.vlte@gmail.com>
2025-02-21 19:25:16 +0100

    Build steam 0.3.5

--- Assets/Scripts/Main Menu/MainMenu.cs
+++ Assets/Scripts/Main Menu/MainMenu.cs
@@ -54,4 +54,12 @@ public class MainMenu : MonoBehaviour
     {
         Application.OpenURL("https://store.steampowered.com/app/1949310/Eternals_Path/");
     }
+    public void OpenDiscordPage()
+    {
+        Application.OpenURL("https://discord.com/invite/naCEPbru");
+    }
+    public void OpenLinktreePage()
+    {
+        Application.OpenURL("https://linktr.ee/sleeplessparadise");
+    }
 }
