e123b0902af67552538078560d239c5f33a16408
NikoMyoji <nicolas.vlte@gmail.com>
2025-02-23 18:01:39 +0100

    Update main menu

--- Assets/Scripts/Main Menu/MainMenu.cs
+++ Assets/Scripts/Main Menu/MainMenu.cs
@@ -53,16 +53,5 @@ public class MainMenu : MonoBehaviour
         _animator.SetTrigger("CharacterBack");
         ShowMainMenu();
     }
-    public void OpenSteamPage()
-    {
-        Application.OpenURL("https://store.steampowered.com/app/1949310/Eternals_Path/");
-    }
-    public void OpenDiscordPage()
-    {
-        Application.OpenURL("https://discord.gg/YvBYf74Paj");
-    }
-    public void OpenLinktreePage()
-    {
-        Application.OpenURL("https://linktr.ee/sleeplessparadise");
-    }
+  
 }
