11a6fdd3cc1c16092371f232f35b07a81dacfb50
NikoMyoji <nicolas.vlte@gmail.com>
2025-03-03 17:18:18 +0100

    Build 0.3.7.1

--- Assets/Scripts/Combat/Behavior/EnnemyBehavior.cs
+++ Assets/Scripts/Combat/Behavior/EnnemyBehavior.cs
@@ -1,10 +1,8 @@
-﻿using JetBrains.Annotations;
-using System;
+﻿using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
-using static UnityEditor.Progress;
 
 public class EnnemyBehavior : CombatBehavior
 {
