d2f23a9cf94d23d7f20513ba2195f29f74528351
NikoMyoji <nicolas.vlte@gmail.com>
2025-04-24 12:06:54 +0200

    Description caméléon. close #175

---
+++ Assets/Scripts/Combat/Passifs/ImplementedPassif/PassiveEffect/DescriptionPassive.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New DescriptionPassive passiv", menuName = "PassiveEffect/New DescriptionPassive passiv")]
+public class DescriptionPassive : AbstractPassive
+{
+    //Pour ajouter une description au passif si il n'ont pas de comportement unique
+}
