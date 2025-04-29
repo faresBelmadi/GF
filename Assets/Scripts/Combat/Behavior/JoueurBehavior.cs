63330aff95a9c02e9f5be08e1145b7fcf48be52d
NikoMyoji <nicolas.vlte@gmail.com>
2025-04-29 18:27:15 +0200

    Fix issue with papy passive

--- Assets/Scripts/Combat/Behavior/JoueurBehavior.cs
+++ Assets/Scripts/Combat/Behavior/JoueurBehavior.cs
@@ -7,6 +7,7 @@ using UnityEngine.UI;
 public class JoueurBehavior : CombatBehavior<JoueurStat>
 {
     public override JoueurStat Stat {
+        get => _stat;
         set
         {
             _stat = value;
