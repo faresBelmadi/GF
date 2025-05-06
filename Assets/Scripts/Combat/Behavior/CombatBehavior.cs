ddc4d8db75ee1c980edf956dbec3189311ea5f2e
NikoMyoji <nicolas.vlte@gmail.com>
2025-05-06 15:42:46 +0200

    Update CombatBehavior.cs

--- Assets/Scripts/Combat/Behavior/CombatBehavior.cs
+++ Assets/Scripts/Combat/Behavior/CombatBehavior.cs
@@ -1,10 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
-using static UnityEditor.Progress;
 
 public abstract class CombatBehavior<T> : MonoBehaviour where T : CharacterStat
 {
