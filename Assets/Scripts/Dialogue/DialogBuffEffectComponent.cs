7dc35f6f89841f0a5a02d0f5f5cee64c89f823ea
NikoMyoji <nicolas.vlte@gmail.com>
2025-05-10 19:57:44 +0200

    Ajout Rotation

--- Assets/Scripts/Dialogue/DialogBuffEffectComponent.cs
+++ Assets/Scripts/Dialogue/DialogBuffEffectComponent.cs
@@ -13,11 +13,21 @@ public class DialogBuffEffectComponent : MonoBehaviour, IPointerEnterHandler, IP
     private Image _targetImage;
     [SerializeField]
     private Image _effectImage;
+    [SerializeField]
+    private float _timebetweenTarget;
+    [SerializeField]
+    private float _rotTime;
 
     private string _name;
     private string _description;
 
     private List<Sprite> _targets;
+
+    private int _currentIndex = 0;
+    private float _currentTime = 0;
+    private float _currentRotTime = 0;
+    private bool _isSpinning = false;
+    private float _currentRot;
 
     private void OnEnable()
     {
@@ -28,12 +38,61 @@ public class DialogBuffEffectComponent : MonoBehaviour, IPointerEnterHandler, IP
         CombatBehavior<CharacterStat>.OnUpdateUI -= UpdateUI;
     }
 
+    private void Start()
+    {
+        _currentIndex = 0;
+        _currentTime = 0;
+        _isSpinning = false;
+    }
+
     // Update is called once per frame
     void Update()
     {
-        
-    }
 
+       
+        if (_targets.Count > 1)
+        {
+            _currentTime += Time.deltaTime;
+            if (!_isSpinning && _currentTime >= _timebetweenTarget)
+            {
+                _isSpinning = true;
+                _currentTime = 0f;
+                StartCoroutine(SpinAndSwap());
+            }
+        }
+    }
+    private IEnumerator SpinAndSwap()
+    {
+        _currentRotTime = 0f;
+        Quaternion startRot = Quaternion.Euler(0f, 0f, 0f);
+        Quaternion endRot = Quaternion.Euler(0f, 90f, 0f);
+        while (_currentRotTime < _rotTime)
+        {
+            _targetImage.transform.parent.rotation = Quaternion.Lerp(startRot, endRot, _currentRotTime / _rotTime);
+            _currentRotTime += Time.deltaTime;
+            yield return null;
+        }
+
+        _targetImage.transform.parent.rotation = Quaternion.Euler(0f, 270f, 0f); ;
+        startRot = _targetImage.transform.parent.rotation;
+        endRot = Quaternion.Euler(0f, 360f, 0f);
+        ChangeTargetSprite();
+        _currentRotTime = 0f;
+        while (_currentRotTime < _rotTime)
+        {
+            _targetImage.transform.parent.rotation = Quaternion.Lerp(startRot, endRot, _currentRotTime / _rotTime);
+            _currentRotTime += Time.deltaTime;
+            yield return null;
+        }
+        _targetImage.transform.parent.rotation = Quaternion.Euler(0f, 0f, 0f);
+        _isSpinning = false;
+        _currentTime = 0f;
+    }
+    private void ChangeTargetSprite()
+    {
+        _currentIndex = (_currentIndex + 1) % _targets.Count;
+        _targetImage.sprite = _targets[_currentIndex];
+    }
     public void SetTargetsSprite(List<Sprite> targetSprites) => SetSprites(null, targetSprites);
     public void SetEffectSprite(Sprite effectSprite) => SetSprites(effectSprite, null);
     public void SetSprites(Sprite effectSprite, List<Sprite> targetSprites)
