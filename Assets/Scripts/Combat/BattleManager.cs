6876785b9ace81dd7b66df5bec95fd70412a4494
NikoMyoji <nicolas.vlte@gmail.com>
2025-05-16 20:23:50 +0200

    fix #135 - boutin fin de tour cliquable
    Fix bouton fin de tour cliquable a des moment ou il devrait pas

--- Assets/Scripts/Combat/BattleManager.cs
+++ Assets/Scripts/Combat/BattleManager.cs
@@ -579,6 +579,13 @@ public class BattleManager : MonoBehaviour
         //turnOrderUIManager.GenerateTurnItems(IdOrder);
     }
 
+    public void ActivatePlayer()
+    {
+        var i = EnemyScripts.Where(x=>x.Stat.Radiance > 0).Count();
+        if (i > 0)
+            player.ActivateSpells();
+    }
+
     #endregion Phase
 
     #region Turn
@@ -1195,6 +1202,7 @@ public class BattleManager : MonoBehaviour
                 }
                 else
                 {
+                    player.DesactivateSpells();
                     StartCoroutine("GatherEssence");
                 }
 
