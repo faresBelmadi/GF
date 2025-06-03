2c6212092f2b9f2b1078554e23c7921ccd7181ee
NikoMyoji <nicolas.vlte@gmail.com>
2025-06-03 15:17:14 +0200

    fix #213

--- Assets/Scripts/Combat/BattleManager.cs
+++ Assets/Scripts/Combat/BattleManager.cs
@@ -695,8 +695,9 @@ public class BattleManager : MonoBehaviour
 
     public void LaunchSpellEnnemi(EnnemiSpell Spell)
     {
-        var playing = EnemyScripts.First(c => c.combatID == currentIdTurn);
-        LogLaunchedSpell(playing.Name, Spell);
+        var playing = EnemyScripts.FirstOrDefault(c => c.combatID == currentIdTurn);
+        if (playing != null)
+            LogLaunchedSpell(playing.Name, Spell);
         foreach (var effet in Spell.Effet)
         {
             PassageEffet(effet, currentIdTurn, -1, SourceEffet.Spell);
