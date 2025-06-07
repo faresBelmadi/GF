650d23319da207d9755340f283e086aff538932c
NikoMyoji <nicolas.vlte@gmail.com>
2025-06-07 17:24:32 +0200

    Last fix

--- Assets/Scripts/Ui/EndGameScreen.cs
+++ Assets/Scripts/Ui/EndGameScreen.cs
@@ -1,9 +1,6 @@
 using System.Collections;
-using System.Collections.Generic;
-using UnityEditorInternal;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.UI.Extensions;
 
 public class EndGameScreen : MonoBehaviour
 {
