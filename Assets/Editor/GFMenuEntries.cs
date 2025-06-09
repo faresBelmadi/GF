c463019923cf0e8b6d4181e85a321fc65f15c38c
Elayan <blue-violin@hotmail.fr>
2025-06-09 16:27:05 +0200

    Validation of imported loca files from toolbar

--- Assets/Editor/GFMenuEntries.cs
+++ Assets/Editor/GFMenuEntries.cs
@@ -4,8 +4,15 @@ using UnityEditor;
 public static class GFMenuEntries
 {
     [MenuItem("Eternal's Path/Import loca files...")]
-    public static void OpenMyPopup()
+    public static void OpenLocaFileImportWindow()
     {
         GgsToCsvImporterWindow.ShowWindow();
+    }
+
+    [MenuItem("Eternal's Path/Validate loca files")]
+    public static void ValidateLocaFiles()
+    {
+        var validator = new GgsToCsvValidator();
+        validator.Validate();
     }
 }
