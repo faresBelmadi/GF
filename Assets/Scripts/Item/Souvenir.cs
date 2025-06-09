bbfb846717a620ff7a0813c84f244b8b48ae4cd9
Elayan <blue-violin@hotmail.fr>
2025-06-09 17:09:55 +0200

    Proper migration of obsolete fields
    (prevent warnings)

--- Assets/Scripts/Item/Souvenir.cs
+++ Assets/Scripts/Item/Souvenir.cs
@@ -14,24 +14,48 @@ public class Souvenir : ScriptableObject
     public int ClassId = 0;
     [SerializeField]
     private string _idTradName;
-    [Tooltip("Default value for name if no traduction provided")]
-    [System.Obsolete("Use SouvenirName instead")]
-    [SerializeField]
-    private string Nom;
     [SerializeField]
     private string _idTradDescription;
-    [Tooltip("Default value for description if no traduction provided")]
-    [TextArea(5, 10)]
-    [System.Obsolete("Use SouvenirDesc instead")]
-    [SerializeField]
-    private string Description;
     public Sprite Icon;
     public Spell SouvenirSpell;
-    
+
+    [Tooltip("Default value for name if no traduction provided")]
+    public string DefaultName;
+
+    [Tooltip("Default value for description if no traduction provided")]
+    [TextArea(5, 10)]
+    public string DefaultDescription;
+
+    #region VERSIONING
+
+    [SerializeField, HideInInspector]
+    private int _dataVersion = 0;
+
+    [System.Obsolete("Use SouvenirDesc instead")]
+    [SerializeField, HideInInspector]
+    private string Description;
+
+    [System.Obsolete("Use SouvenirName instead")]
+    [SerializeField, HideInInspector]
+    private string Nom;
+
+#pragma warning disable CS0618 // Warning for Obsolete field usage
+    public void OnAfterDeserialize()
+    {
+        if (_dataVersion < 1)
+        {
+            DefaultDescription = Description;
+            DefaultName = Nom;
+            _dataVersion = 1;
+        }
+    }
+#pragma warning restore CS0618 // Warning for Obsolete field usage
+
+    #endregion
 
 
-    public string SouvenirName { get { return TradManager.instance.GetTranslation(_idTradName, Nom); } }
-    public string SouvenirDesc { get { return TradManager.instance.GetTranslation(_idTradDescription, Description); } }
+    public string SouvenirName { get { return TradManager.instance.GetTranslation(_idTradName, DefaultName); } }
+    public string SouvenirDesc { get { return TradManager.instance.GetTranslation(_idTradDescription, DefaultDescription); } }
 
     private void Awake()
     {
