56e4c3ba3b22f7827731b94ef3dbd56ad9ff2372
NikoMyoji <nicolas.vlte@gmail.com>
2025-06-26 16:33:20 +0200

    Clean up

--- Assets/Scripts/Main Menu/MainMenu.cs
+++ Assets/Scripts/Main Menu/MainMenu.cs
@@ -34,12 +34,9 @@ public class MainMenu : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //on réinitialise l'animator
-
         _welcomeText.SetActive(true);
         _wipText.SetActive(false);
         _mainMenu.SetActive(false);
-       // _optionMenu.SetActive(false);
     }
 
     // Update is called once per frame
