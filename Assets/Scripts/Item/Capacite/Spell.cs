20540dab336ccba5fcbe0b7036d20f0861b73b55
Elayan <blue-violin@hotmail.fr>
2025-06-26 15:17:46 +0200

    TradId in Spell

--- Assets/Scripts/Item/Capacite/Spell.cs
+++ Assets/Scripts/Item/Capacite/Spell.cs
@@ -1,15 +1,17 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 [System.Serializable]
 [CreateAssetMenu(fileName = "New spell", menuName = "Capacité/Create New Spell", order = 11)]
 public class Spell : ScriptableObject, IBattleLogSpell
 {
-    public string Nom;
-    public string idTradName;
-    [TextArea(5, 10)]
-    public string Description;
-    public string idTradDescription;
+    [SerializeField]
+    public TradId TitleId;
+
+    [SerializeField, DefaultTextStyle(TradIdDefaultTextStyles.TextArea)]
+    public TradId DescriptionId;
+
     public int IDSpell;
     public List<int> IDChildren;
     public SpellStatus SpellStatue;
@@ -26,5 +28,39 @@ public class Spell : ScriptableObject, IBattleLogSpell
 
     public AudioClip SpellSFX => _spellSFX;
 
-    public string TradName => TradManager.instance.GetTranslation(idTradName);
+    public string TradName => TradManager.instance.GetTranslation(TitleId.Id);
+
+    #region VERSIONING
+
+    [SerializeField, HideInInspector]
+    private int _dataVersion = 0;
+
+    [SerializeField, HideInInspector, Obsolete("Use TitleId instead")]
+    public string idTradName;
+    [SerializeField, HideInInspector, Obsolete("Use TitleId instead")]
+    public string Nom;
+
+    [SerializeField, HideInInspector, Obsolete("Use DescriptionId instead")]
+    public string idTradDescription;
+    [SerializeField, HideInInspector, Obsolete("Use DescriptionId instead")]
+    public string Description;
+
+#pragma warning disable CS0618 // Warning for Obsolete field usage
+    [ExecuteAlways]
+    public void OnEnable()
+    {
+        if (_dataVersion < 1)
+        {
+            TitleId.Id = idTradName;
+            TitleId.DefaultText = Nom;
+
+            DescriptionId.Id = idTradDescription;
+            DescriptionId.DefaultText = Description;
+
+            _dataVersion = 1;
+        }
+    }
+#pragma warning restore CS0618 // Warning for Obsolete field usage
+
+    #endregion
 }
