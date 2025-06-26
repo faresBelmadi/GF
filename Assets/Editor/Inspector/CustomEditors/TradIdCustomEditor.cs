8f9a3fe0c7c10697d8e9ed6bc3ab63f04d8c88e7
Elayan <blue-violin@hotmail.fr>
2025-06-26 15:17:37 +0200

    TradId custom editor and default text area attribute

--- Assets/Editor/Inspector/CustomEditors/TradIdCustomEditor.cs
+++ Assets/Editor/Inspector/CustomEditors/TradIdCustomEditor.cs
@@ -1,7 +1,7 @@
+using System.Linq;
+using System.Reflection;
 using UnityEditor;
 using UnityEngine;
-using UnityEngine.UIElements;
-using static Codice.CM.Common.CmCallContext;
 
 [CustomPropertyDrawer(typeof(TradId))]
 public class TradIdCustomEditor : PropertyDrawer
@@ -24,12 +24,29 @@ public class TradIdCustomEditor : PropertyDrawer
             string newId = EditorGUILayout.TextField(idProp.displayName, idProp.stringValue);
             idProp.stringValue = newId;
 
-            SerializedProperty defaultTextProp = property.FindPropertyRelative(nameof(TradId.DefaultText));
-            defaultTextProp.stringValue = EditorGUILayout.TextField(defaultTextProp.displayName, defaultTextProp.stringValue);
+            DrawDefaultText(property);
 
             string previewText = _tradMgr.GetRawTranslations(newId, out var idIsUnknown);
             EditorGUILayout.HelpBox(previewText, idIsUnknown ? MessageType.Warning : MessageType.Info);
         }
         EditorGUI.indentLevel--;
+    }
+
+    private void DrawDefaultText(SerializedProperty property)
+    {
+        var defaultTextStyleAttributes = fieldInfo.GetCustomAttributes<DefaultTextStyleAttribute>(true);
+        var defaultTextStyle = defaultTextStyleAttributes.FirstOrDefault()?.Style ?? TradIdDefaultTextStyles.TextField;
+
+        SerializedProperty defaultTextProp = property.FindPropertyRelative(nameof(TradId.DefaultText));
+
+        if (defaultTextStyle == TradIdDefaultTextStyles.TextArea)
+        {
+            EditorGUILayout.LabelField(defaultTextProp.displayName);
+            defaultTextProp.stringValue = EditorGUILayout.TextArea(defaultTextProp.stringValue, EditorStyles.textArea, GUILayout.MinHeight(50));
+        }
+        else
+        {
+            defaultTextProp.stringValue = EditorGUILayout.TextField(defaultTextProp.displayName, defaultTextProp.stringValue);
+        }
     }
 }
