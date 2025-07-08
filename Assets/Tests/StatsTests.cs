9a66dd74402b9931d44a95b9736c46925f9565a4
NikoMyoji <nicolas.vlte@gmail.com>
2025-07-08 14:46:26 +0200

    Add basics tests

--- Assets/Tests/StatsTests.cs
+++ Assets/Tests/StatsTests.cs
@@ -1,10 +1,11 @@
 using NUnit.Framework;
+using System;
 using UnityEngine;
 
 public class StatsTests
 {
 
-    public CharacterStat CreateStat(int initialValue)
+    public CharacterStat CreateStatSO(int initialValue)
     {
         CharacterStat stat = ScriptableObject.CreateInstance<CharacterStat>();
 
@@ -19,12 +20,12 @@ public class StatsTests
 
         return stat;
     }
-
+    #region TEST SO
     [Test]
     public void ModifStatUp()
     {
-        CharacterStat stat = CreateStat(0);
-        CharacterStat modifStat = CreateStat(10);
+        CharacterStat stat = CreateStatSO(0);
+        CharacterStat modifStat = CreateStatSO(10);
         
 
         stat.ModifStateAll(modifStat);
@@ -40,8 +41,8 @@ public class StatsTests
     [Test]
     public void ModifStatDown()
     {
-        CharacterStat stat = CreateStat(0);
-        CharacterStat modifStat = CreateStat(- 20);
+        CharacterStat stat = CreateStatSO(0);
+        CharacterStat modifStat = CreateStatSO(- 20);
 
 
         stat.ModifStateAll(modifStat);
@@ -57,8 +58,8 @@ public class StatsTests
     [Test]
     public void RemoveStatUp()
     {
-        CharacterStat stat = CreateStat(0);
-        CharacterStat modifStat = CreateStat(-10);
+        CharacterStat stat = CreateStatSO(0);
+        CharacterStat modifStat = CreateStatSO(-10);
 
 
         stat.removeStat(modifStat);
@@ -74,8 +75,8 @@ public class StatsTests
     [Test]
     public void RemoveStatDown()
     {
-        CharacterStat stat = CreateStat(0);
-        CharacterStat modifStat = CreateStat(20);
+        CharacterStat stat = CreateStatSO(0);
+        CharacterStat modifStat = CreateStatSO(20);
 
 
         stat.removeStat(modifStat);
@@ -92,7 +93,7 @@ public class StatsTests
     [Test]
     public void ResetStat()
     {
-        CharacterStat stat = CreateStat(0);
+        CharacterStat stat = CreateStatSO(0);
 
         stat.ResetStat();
 
@@ -104,5 +105,79 @@ public class StatsTests
         Assert.AreEqual(stat.Radiance, 0);
         Assert.AreEqual(stat.RadianceMax, -0);
     }
+    #endregion
+    #region TEST STATSHOLDER
+    [Test]
+    public void ConstructorStatsHolder()
+    {
+        var baseStat = CreateStatSO(0);
+        
+        var statHolder = new StatsHolder(baseStat);
+        
+        Assert.That(statHolder.Calme, Is.EqualTo(baseStat.Calme));
+        Assert.That(statHolder.Conviction, Is.EqualTo(baseStat.Conviction));
+        Assert.That(statHolder.Resilience, Is.EqualTo(baseStat.Resilience));
+        Assert.That(statHolder.Essence, Is.EqualTo(baseStat.Essence));
+        Assert.That(statHolder.ForceAme, Is.EqualTo(baseStat.ForceAme));
+        Assert.That(statHolder.Radiance, Is.EqualTo(baseStat.Radiance));
+        Assert.That(statHolder.RadianceMax, Is.EqualTo(baseStat.RadianceMax));
 
+        Assert.That(statHolder.BaseCalme, Is.EqualTo(baseStat.Calme));
+        Assert.That(statHolder.BaseConviction, Is.EqualTo(baseStat.Conviction));
+        Assert.That(statHolder.BaseResilience, Is.EqualTo(baseStat.Resilience));
+        Assert.That(statHolder.BaseForceDame, Is.EqualTo(baseStat.ForceAme));
+        Assert.That(statHolder.BaseRadianceMax, Is.EqualTo(baseStat.RadianceMax));
+    }
+    [Test]
+    public void UpdateStatsHolder()
+    {
+        int baseValue = 0;
+        var baseStat = CreateStatSO(baseValue);
+        var modifStat = CreateStatSO(10);
+
+        var statHolder = new StatsHolder(baseStat);
+        statHolder.UpdateStat(modifStat);
+
+        Assert.That(statHolder.Calme, Is.EqualTo(10));
+        Assert.That(statHolder.Conviction, Is.EqualTo(10));
+        Assert.That(statHolder.Resilience, Is.EqualTo(10));
+        Assert.That(statHolder.Essence, Is.EqualTo(10));
+        Assert.That(statHolder.ForceAme, Is.EqualTo(10));
+        Assert.That(statHolder.Radiance, Is.EqualTo(10));
+        Assert.That(statHolder.RadianceMax, Is.EqualTo(10));
+
+        Assert.That(statHolder.Calme, Is.Not.EqualTo(statHolder.BaseCalme));
+        Assert.That(statHolder.Conviction, Is.Not.EqualTo(statHolder.BaseConviction));
+        Assert.That(statHolder.Resilience, Is.Not.EqualTo(statHolder.BaseResilience));
+        Assert.That(statHolder.ForceAme, Is.Not.EqualTo(statHolder.BaseForceDame));
+        Assert.That(statHolder.RadianceMax, Is.Not.EqualTo(statHolder.BaseRadianceMax));
+
+        Assert.That(statHolder.BaseCalme, Is.EqualTo(baseValue));
+        Assert.That(statHolder.BaseConviction, Is.EqualTo(baseValue));
+        Assert.That(statHolder.BaseResilience, Is.EqualTo(baseValue));
+        Assert.That(statHolder.BaseForceDame, Is.EqualTo(baseValue));
+        Assert.That(statHolder.BaseRadianceMax, Is.EqualTo(baseValue));
+    }
+    [Test]
+    public void ResetStatHolder()
+    {
+        int baseValue = 0;
+        var baseStat = CreateStatSO(baseValue);
+        var modifStat = CreateStatSO(10);
+
+        var statHolder = new StatsHolder(baseStat);
+        statHolder.UpdateStat(modifStat);
+        statHolder.ResetStat();
+
+        Assert.That(statHolder.Calme, Is.EqualTo(baseValue));
+        Assert.That(statHolder.Conviction, Is.EqualTo(baseValue));
+        Assert.That(statHolder.Resilience, Is.EqualTo(baseValue));
+        Assert.That(statHolder.ForceAme, Is.EqualTo(baseValue));
+        Assert.That(statHolder.Radiance, Is.EqualTo(baseValue));
+        Assert.That(statHolder.RadianceMax, Is.EqualTo(baseValue));
+
+       
+    }
+            
+    #endregion
 }
