33091150210e9dcffaed744f7bac81871d06956c
NikoMyoji <nicolas.vlte@gmail.com>
2025-08-06 18:59:32 +0200

    Add unit test

--- Assets/Scripts/Item/Capacite/Effet.cs
+++ Assets/Scripts/Item/Capacite/Effet.cs
@@ -520,11 +520,6 @@ public class Effet : ScriptableObject
                     Mathf.FloorToInt((((Pourcentage / 100f) * NbAttaque) * Cible.Radiance) *
                                      Caster.MultiplDegat); //checker le multipl degat
                 Caster.ChangeRadiance(amountPonction);
-                /* old
-                 * Caster.Radiance += amountPonction;
-                if (Caster.Radiance > Caster.RadianceMax)
-                    Caster.Radiance = Caster.RadianceMax;
-                */
                 ModifState.Radiance += -amountPonction;
                 break;
             case TypeEffet.DamageAllEvenly:
@@ -553,11 +548,6 @@ public class Effet : ScriptableObject
                     Mathf.FloorToInt((((Pourcentage / 100f) * NbAttaque) * Caster.ForceAme) *
                                      Caster.MultiplDegat); //checker le multipl degat
                 Caster.ChangeRadiance(amountPonctionFA);
-               /*Old
-                Caster.Radiance += amountPonctionFA;
-                if (Caster.Radiance > Caster.RadianceMax)
-                    Caster.Radiance = Caster.RadianceMax;
-                */
                 ModifState.Radiance += -amountPonctionFA;
                 break;
             case TypeEffet.PremiereAttaqueJeanne:
