f4c42a1e9387e152475493c8f1305710719ecd36
NikoMyoji <nicolas.vlte@gmail.com>
2025-12-05 15:34:12 +0100

    Add ApplyEffect Debug Panel

--- Assets/Scripts/Debug/DebugStatPanel.cs
+++ Assets/Scripts/Debug/DebugStatPanel.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
@@ -6,6 +7,7 @@ public class DebugStatPanel : MonoBehaviour
     [SerializeField] private GameObject _debugPanel;
     [SerializeField] private TMP_Text _faValueText;
     [SerializeField] private TMP_Text _radValueText;
+    [SerializeField] private TMP_Text _radMaxValueText;
     [SerializeField] private TMP_Text _resValueText;
     [SerializeField] private TMP_Text _vitValueText;
     [SerializeField] private TMP_Text _convicValueText;
@@ -14,11 +16,20 @@ public class DebugStatPanel : MonoBehaviour
     [SerializeField] private TMP_Text _conscienceMaxValueText;
     [SerializeField] private TMP_Text _clairvoyanceValueText;
 
+    [SerializeField] private TMP_Dropdown _dropdown;
+    [SerializeField] private List<Effet> _effectList;
 
+    private Dictionary<string, Effet> _dictionnaryEffect = new Dictionary<string, Effet>();
     // Start is called before the first frame update
     void Start()
     {
-
+        List<string> effectNames = new List<string>();
+        foreach (var effet in _effectList)
+        {
+            effectNames.Add(effet.name);
+            _dictionnaryEffect.Add(effet.name, ScriptableObject.Instantiate<Effet>(effet));
+        }
+        _dropdown.AddOptions(effectNames);
     }
 
     // Update is called once per frame
@@ -27,7 +38,8 @@ public class DebugStatPanel : MonoBehaviour
         if (GameManager.Instance.playerStatHandler != null)
         {
             _faValueText.text = GameManager.Instance.playerStatHandler.ForceDameTotal.ToString();
-            _radValueText.text = GameManager.Instance.playerStatHandler.RadianceMaxTotal.ToString();
+            _radValueText.text = GameManager.Instance.playerStatHandler.Radiance.ToString();
+            _radMaxValueText.text = GameManager.Instance.playerStatHandler.RadianceMaxTotal.ToString();
             _resValueText.text = GameManager.Instance.playerStatHandler.ResilienceTotal.ToString();
             _vitValueText.text = GameManager.Instance.playerStatHandler.VitesseTotal.ToString();
             _convicValueText.text = GameManager.Instance.playerStatHandler.ConvictionTotal.ToString();
@@ -41,6 +53,14 @@ public class DebugStatPanel : MonoBehaviour
     public void TogglePanel()
     {
         _debugPanel.SetActive(!_debugPanel.activeSelf);
+    }
+    public void ApplyEffect()
+    {
+        string name = _dropdown.options[_dropdown.value].text;
+        Effet EffectToApply = _dictionnaryEffect[name];
+        if (EffectToApply == null) return;
+        GameManager.Instance.playerStatHandler.UpdateStat(EffectToApply.ResultEffet(GameManager.Instance.playerStatHandler, Cible: GameManager.Instance.playerStatHandler));
+
     }
     
 }
