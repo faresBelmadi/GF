fecfa722c92343748a68238c1a9bdba5bd5dca98
NikoMyoji <nicolas.vlte@gmail.com>
2026-03-26 10:34:57 +0100

    cleanup before test build

--- Assets/Scripts/PulseBloom_System.cs
+++ Assets/Scripts/PulseBloom_System.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Globalization;
 using TMPro;
-using Unity.Plastic.Newtonsoft.Json.Linq;
 using UnityEngine;
 
 using UnityEngine.UI;
